bef41c9067f26249a0a9ed745aa82012c7d198d9
theblind <ruanbowen@gmail.com>
2013-04-26 15:47:01 +0800

    update new assianment

---
+++ PGM_Programming_Assignment_1/ComputeJointDistribution.m
@@ -0,0 +1,36 @@
+%ComputeJointDistribution Computes the joint distribution defined by a set
+% of given factors
+%
+%   Joint = ComputeJointDistribution(F) computes the joint distribution
+%   defined by a set of given factors
+%
+%   Joint is a factor that encapsulates the joint distribution given by F
+%   F is a vector of factors (struct array) containing the factors 
+%     defining the distribution
+%
+
+function Joint = ComputeJointDistribution(F)
+
+  % Check for empty factor list
+  if (numel(F) == 0)
+      warning('Error: empty factor list');
+      Joint = struct('var', [], 'card', [], 'val', []);      
+      return;
+  end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE:
+% Compute the joint distribution defined by F
+% You may assume that you are given legal CPDs so no input checking is required.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Joint = F(1);
+n = size(F, 2);
+for i = 2:n
+    Joint = FactorProduct(Joint, F(i));
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
+
